7e8ac1ee50f677aa215a166d21021bfd1471f4d4
AlexMcDermott <alexjmcd2202@gmail.com>
2021-11-11 22:56:40 +1000

    Can now load a new rom without reloading first

--- src/ts/keyboard.ts
+++ src/ts/keyboard.ts
@@ -5,27 +5,31 @@ export default class Keyboard {
 
   constructor() {
     this.keymap = {
-      '1': 0x1, // 1 1
-      '2': 0x2, // 2 2
-      '3': 0x3, // 3 3
-      '4': 0xc, // 4 C
-      'q': 0x4, // q 4
-      'w': 0x5, // w 5
-      'e': 0x6, // e 6
-      'r': 0xd, // r D
-      'a': 0x7, // a 7
-      's': 0x8, // s 8
-      'd': 0x9, // d 9
-      'f': 0xe, // f E
-      'z': 0xa, // z A
-      'x': 0x0, // x 0
-      'c': 0xb, // c B
-      'v': 0xf, // v F
+      '1': 0x1,
+      '2': 0x2,
+      '3': 0x3,
+      '4': 0xc,
+      'q': 0x4,
+      'w': 0x5,
+      'e': 0x6,
+      'r': 0xd,
+      'a': 0x7,
+      's': 0x8,
+      'd': 0x9,
+      'f': 0xe,
+      'z': 0xa,
+      'x': 0x0,
+      'c': 0xb,
+      'v': 0xf,
     };
 
+    window.addEventListener('keyup', this.keyUp.bind(this));
+    this.clear();
+  }
+
+  public clear() {
     this.pressed = new Array(16).fill(false);
     this.callback = null;
-    window.addEventListener('keyup', this.keyUp.bind(this));
   }
 
   public isPressed(code: number) {
